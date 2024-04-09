9378326e012eab3fddeb4ddf9206a617717f536f
MFellen <m_fellen@live.concordia.ca>
2024-04-08 22:05:02 -0400

    fixing test for operator console

--- ATC/src/ATC.cpp
+++ ATC/src/ATC.cpp
@@ -68,7 +68,7 @@ void *writeToFile(void* arg) {
 		while (true) {
 			time = count * period_sec;
 
-			if ((time % 30) == 0) {
+			if ((time % 20) == 0) {
 				for (Aircraft *currentAircraft: *myList) {
 					info = "Aircraft ID: " + to_string(currentAircraft->getId()) +
 						   "\nAltitude: " + to_string(currentAircraft->getZ()) +
@@ -108,12 +108,13 @@ int main() {
     OperatorConsole oc(&aircraft1);
     System system(acVec, &radar, &oc);
     // Create two threads
-    pthread_t thread1, thread2, thread3, rThread, sThread, ocThread;
+    pthread_t thread1, thread2, thread3, rThread, sThread, ocThread, writeThread;
 
     // Start the threads, passing each thread its corresponding aircraft
     pthread_create(&thread1, NULL, aircraftThread, &aircraft1);
 //    pthread_create(&thread2, NULL, aircraftThread, &aircraft2);
 //    pthread_create(&thread3, NULL, aircraftThread, &aircraft3);
+    pthread_create(&writeThread, NULL, writeToFile, &acVec);
     pthread_create(&rThread, NULL, radarThread, &radar);
     pthread_create(&sThread, NULL, systemThread, &system);
     pthread_create(&ocThread, NULL, operatorConsoleThread, &oc);
@@ -124,17 +125,7 @@ int main() {
 //    pthread_join(thread3, NULL);
     pthread_join(rThread, NULL);
     pthread_join(ocThread, NULL);
-
-
-    // Testing Operating Console
-    pthread_t ocThread, writeThread;
-    OperatorConsole oc(aircraft1);
-
-    pthread_create(&ocThread, NULL, operatorConsoleThread, &oc);
-    pthread_create(&writeThread, NULL, writeToFile, &acVec);
-    pthread_join(ocThread, NULL);
     pthread_join(writeThread, NULL);
-
     return 0;
 }
 
