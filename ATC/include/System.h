c9c7806e5d3d6da82af4a0fc295be9ae09a53942
mikenkie <ineke.kim@gmail.com>
2024-04-10 09:52:24 -0400

    read input file

--- ATC/include/System.h
+++ ATC/include/System.h
@@ -11,16 +11,28 @@
 #include <vector>
 #include "Radar.h"
 #include "OperatorConsole.h"
+
+
 class System {
 private:
 	vector<Aircraft*> aircraftList;
 	Radar* radar;
 	OperatorConsole* opCon;
+
+	// This is used to generate data of aircrafts from input file
+	typedef struct _my_aircraft {
+		int entrancetime;
+		int ID;
+		float coordx, coordy, coordz;
+		float speedx, speedy, speedz;
+	} my_aircraft_t;
+
 public:
 	System();
 	System(vector<Aircraft*> acList, Radar* r, OperatorConsole* oc);
 	virtual ~System();
 	void simulate();
+	void read_aircrafts_from_input_file();
 };
 
 #endif /* SRC_SYSTEM_H_ */
--- ATC/src/System.cpp
+++ ATC/src/System.cpp
@@ -14,6 +14,14 @@
 #include "cTimer.h"
 #include <cmath>
 
+// All for reading input file
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
 using namespace std;
 
 System::System() {
@@ -28,6 +36,93 @@ System::System(vector<Aircraft*> acList, Radar *r, OperatorConsole *oc) {
 
 System::~System() {
 	// TODO Auto-generated destructor stub
+}
+
+void System::read_aircrafts_from_input_file() {
+	my_aircraft_t aircraft_values;
+	int input_file;
+	int size_read;
+	char buffer[305]; // Manually adjust the size of the buffer according to the input file
+	int index = 0;
+
+	int reset = 1; // The number of lines in text file that defines an aircraft
+
+	/*
+	 * Open a file for input but replace file you want to read with the path.
+	 * For testing, we have four files: input_low, input_medium, input_high, input_overload.
+	 */
+	input_file = open( "input_low.txt", O_RDONLY );
+
+
+    // Read characters from file until end of file is encountered
+    while ((size_read = read(input_file, &buffer[index], 1)) > 0) {
+    	// If the reader reaches the end of the line, else continues to add character to buffer
+        if (buffer[index] == '\n' || index == sizeof(buffer) - 1) {
+            // Print content read up to newline character or end of buffer
+            buffer[index] = '\0'; // Null-terminate the string
+            std::cout << "Line: " << buffer << std::endl; //Debug message
+
+            /* Test for error */
+            if( size_read == -1 )
+                perror( "Error reading input file." );
+
+            // Extract values
+            std::string data;
+
+            int i =0;
+            for (i = 0; buffer[i] != '\0'; ++i) {
+            	if (buffer[i] == ' ') {
+            		break;
+            	}
+            }
+
+            for (int j = i+1; buffer[j] != '\0'; ++j) {
+            	data += buffer[j];
+            }
+
+            switch (reset) {
+            case 1: // Time
+            	aircraft_values.entrancetime = stoi(data);
+            	break;
+            case 2: // Aircraft ID
+            	aircraft_values.ID = stoi(data);
+            	break;
+            case 3: // X
+            	aircraft_values.coordx = stof(data);
+            	break;
+            case 4: // Y
+            	aircraft_values.coordy = stof(data);
+            	break;
+            case 5: // Z
+            	aircraft_values.coordz = stof(data);
+            	break;
+            case 6: // Speed X
+            	aircraft_values.speedx = stof(data);
+            	break;
+            case 7: // Speed Y
+            	aircraft_values.speedy = stof(data);
+            	break;
+            case 8: // Speed Z
+            	aircraft_values.speedz = stof(data);
+
+            	Aircraft new_aircraft(aircraft_values.ID, aircraft_values.entrancetime,
+            			aircraft_values.coordx, aircraft_values.coordy, aircraft_values.coordz,
+						aircraft_values.speedx,aircraft_values.speedy, aircraft_values.speedz);
+
+            	aircraftList.push_back(&new_aircraft);
+            	reset = 1; // Resets next block of data to make aircraft object
+            	break;
+            }
+            reset++;
+            index = 0; // Reset index for next line
+        }
+        else {
+            ++index; // Increment index
+        }
+    }
+
+	/* Close the file */
+	close( input_file );
 }
 
 void System::simulate() {
